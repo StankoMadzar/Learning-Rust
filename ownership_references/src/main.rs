4d8d8798578e542061984b312194183840f52b1e
Stanko Madzar <stanko.madzar@outlook.com>
2022-12-09 16:34:47 +0100

    Unfinished Section 4 of the Rust course

---
+++ ownership_references/src/main.rs
@@ -0,0 +1,62 @@
+// Silence some warnings so they don't distract from the exercise.
+#![allow(unused_mut, unused_variables)]
+
+fn inspect (argument: &String) {
+    if argument.ends_with("s") {
+        println!("plural boi");
+    } else {
+        println!("singular boi");
+    }
+}
+
+fn change(argument : &mut String) {
+    if !argument.ends_with("s") {
+        argument.push_str("s");
+        println!("The argument is now {}", argument);
+    }
+}
+
+fn eat (argument : String) -> bool {
+    if argument.starts_with("b") && argument.contains("a") {
+        true
+    } else {
+        false
+    }
+}
+
+fn bedazzle (argument : &mut String) {
+    (argument*). 
+    // Ovde si stao 
+}
+
+fn main() {
+    // This fancy stuff either gets the first argument as a String, or prints
+    // usage and exits if an argument was not supplied to the program.
+    let mut arg: String = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| {
+        println!("Please supply an argument to this program.");
+        std::process::exit(-1);
+    }).to_owned();
+
+    inspect(&arg);
+    change(&mut arg);
+    if eat(arg) {
+        println!("Might be bananas");
+    } else {
+        println!("Not bananas");
+    }
+
+
+    // Challenge: Write a function "bedazzle" that takes a mutable reference to a String and
+    // ignores what is in the string and replaces the contents of the string with the String
+    // "sparkly". Then uncomment the code below.
+    //
+    // Hint: You will need to dereference the mutable reference in order to assign it a
+    // new value.
+    //
+    // let mut material = "mud".to_string();
+    // println!("This material is just `{}`.", material);
+    // bedazzle(&mut material);
+    // println!("Wow! Now the material is `{}`!", material);
+}
